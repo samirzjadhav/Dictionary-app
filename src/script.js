0a21999dc6f461611a40d1152acf9054ed0bf790
Samir Jadhav <samirzjadhav@gmail.com>
2023-08-31 20:52:54 +0530

    feat: add script on btn g

--- src/script.js
+++ src/script.js
@@ -2,3 +2,13 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+  let inpWord = document.getElementById("inp-word").value;
+  console.log(inpWord);
+  fetch(`${url}${inpWord}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
